75c57d4c7b8753eaf5ef3b3fb8115c26f6cd6ce0
Mohamad Erfan <130228115+ErfanDev88@users.noreply.github.com>
2023-10-07 15:06:00 +0330

    Add buttons for hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { useCount } from "./useCount"
 
 function App() {
-
+    const {count, increase, decrease, reset} = useCount()
   return (
     <>
-      <h1></h1>
-      <button>+</button>
-      <button>-</button>
-      <button>Reset</button>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={reset}>Reset</button>
     </>
   )
 }
